1176acd2e798a459f48b91d98ff3e6539f75b492
Alek Mieczkowski <alekmieczkowski@gmail.com>
2017-02-24 12:06:30 -0500

    added movies routing

--- myrottenpotatoes/config/routes.rb
+++ myrottenpotatoes/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+end
+
+  
+Myrottenpotatoes::Application.routes.draw do
+  resource :movies
+  root :to => redirect('/movies')
 end
